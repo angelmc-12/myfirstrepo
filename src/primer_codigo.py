3de1485693dd8d52adf8b8071a1b71d3cb29dc67
Angel Mayta <angelmaytacoaguila@gmail.com>
2024-12-29 21:10:54 -0500

    actualizacion del file python en master

--- src/primer_codigo.py
+++ src/primer_codigo.py
@@ -4,5 +4,5 @@ import pandas as pd
 import numpy as np
 import os
 
-df = pd.read_csv('./data/Dataset Endeudamiento Crediticio.csv',sep=';')
+dfmaster = pd.read_csv('./data/Dataset Endeudamiento Crediticio.csv',sep=';')
 
